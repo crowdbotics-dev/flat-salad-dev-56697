6aaa94dcda7e46e97b1b7ce9cc2c69b6cca1f476
Crowdbotics <engineering@crowdbotics.com>
2023-01-13 10:03:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-salad-dev-56697",
+  url: "https://flat-salad-dev-56697.botics.co",
+  api: "https://flat-salad-dev-56697.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
